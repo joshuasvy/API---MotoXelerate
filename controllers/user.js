0a0b6f9bf461a73df5f7952aa49e85ce6dd624b4
joshuasvy <joshuapaulcortez@gmail.com>
2025-10-25 23:58:47 +0800

    API

---
+++ controllers/user.js
@@ -0,0 +1,21 @@
+import express from "express";
+import User from "./models/Users.js";
+
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required." });
+    }
+
+    const newUser = new User({ email, password });
+    await newUser.save();
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
