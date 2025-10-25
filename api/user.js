5f20da11c2d19db1b9f27590c57028d387d57e92
joshuasvy <joshuapaulcortez@gmail.com>
2025-10-26 03:25:09 +0800

    Login Testing

--- api/user.js
+++ api/user.js
@@ -1,8 +1,13 @@
 import express from "express";
 import Users from "../models/Users.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Register Crud
 router.post("/register", async (req, res) => {
   console.log("📥 Received from frontend:", req.body);
   try {
@@ -22,6 +27,27 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// Login Crud
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Users.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "7d" });
+
+    res.json({ message: "Login successful", token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Fetching all users
 router.get("/all", async (req, res) => {
   try {
     const users = await Users.find();
--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const usersSchema = new mongoose.Schema({
   name: { type: String, required: true },
   contact: { type: Number, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+});
+
+// Password hashing
+usersSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 export default mongoose.model("Users", usersSchema);
