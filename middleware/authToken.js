948b0771758b112afe31a081c995dcc2e62ff8a2
joshuasvy <joshuapaulcortez@gmail.com>
2025-10-28 03:19:41 +0800

    delete function

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,5 +1,12 @@
 import jwt from "jsonwebtoken";
 
+const user = await Users.findById(decoded.id);
+req.user = {
+  id: user._id,
+  role: user.role,
+  name: user.name,
+};
+
 export const authToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "No token provided" });
