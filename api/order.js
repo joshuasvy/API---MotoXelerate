bdc8427f2367a88db4814874490e4013c73146d9
joshuasvy <joshuapaulcortez@gmail.com>
2025-10-31 03:49:05 +0800

    eto na yun??

--- api/order.js
+++ api/order.js
@@ -82,6 +82,17 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Orders.find().sort({ createdAt: -1 });
+    res.status(200).json(orders);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch orders", error: err.message });
+  }
+});
+
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
