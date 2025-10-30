d0339a46f630242e35d6487d6de608d0d62bab1f
joshuasvy <joshuapaulcortez@gmail.com>
2025-10-31 03:30:06 +0800

    haha

--- api/order.js
+++ api/order.js
@@ -82,12 +82,18 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const orders = await Orders.find().sort({ createdAt: -1 });
-    res.status(200).json(orders);
+    const order = await Orders.findById(id);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    res.status(200).json(order);
   } catch (err) {
-    res.status(500).json({ error: "Failed to fetch orders" });
+    res.status(500).json({ message: "Server error", error: err.message });
   }
 });
 
