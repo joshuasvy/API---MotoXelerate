ae6da8fcda4518c6728bf2475a9481e975e74982
joshuasvy <joshuapaulcortez@gmail.com>
2025-10-31 02:32:54 +0800

    fetching products

--- api/order.js
+++ api/order.js
@@ -91,4 +91,25 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Express route
+router.put("/api/order/:id", async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  try {
+    const updatedOrder = await Orders.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+
+    if (!updatedOrder)
+      return res.status(404).json({ message: "Order not found" });
+
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 export default router;
