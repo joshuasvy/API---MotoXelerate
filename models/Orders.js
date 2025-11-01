43df3b83d5f6f9d553b221f163c73536fac50a99
joshuasvy <joshuapaulcortez@gmail.com>
2025-11-01 09:02:33 +0800

    new orderItemSchema

--- models/Orders.js
+++ models/Orders.js
@@ -6,12 +6,7 @@ const orderItemSchema = new mongoose.Schema({
     ref: "Product",
     required: true,
   },
-  productName: { type: String, required: true },
-  image: { type: String, required: true },
   quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
-  specification: { type: String },
-  category: { type: String, required: true },
   status: {
     type: String,
     enum: ["Processing", "Shipped", "Delivered", "Cancelled"],
