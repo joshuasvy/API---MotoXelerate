fdcc5bede6bce6d70a6e00922249c437809675a9
joshuasvy <joshuapaulcortez@gmail.com>
2025-11-14 13:01:49 +0800

    buwan

--- models/Orders.js
+++ models/Orders.js
@@ -12,11 +12,6 @@ const orderItemSchema = new mongoose.Schema({
     enum: ["For approval", "To ship", "Ship", "Delivered", "Completed"],
     default: "For approval",
   },
-  read: {
-    type: Boolean,
-    default: false,
-    required: true,
-  },
 });
 
 const orderSchema = new mongoose.Schema(
